1f328dc1d31f74122dbd10acccb25382dde5ec84
Shaurya11243 <142816890+Shaurya11243@users.noreply.github.com>
2026-07-22 16:26:46 +0530

    Time: 0 ms (100%), Space: 31.7 MB (99.2%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,38 +1,25 @@
-class Solution {
+class Solution { 
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        queue<pair<int, int>> q;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({i, j});
-                } else {
-                    mat[i][j] = INT_MAX;
-                }
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+        int m = mat.size(), n = mat[0].size(), INF = m + n; 
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
             }
         }
-
-        while (!q.empty()) {
-            pair<int, int> cell = q.front();
-            q.pop();
-            int row = cell.first;
-            int col = cell.second;
-
-            for (pair<int, int> direction : directions) {
-                int newRow = row + direction.first;
-                int newCol = col + direction.second;
-
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && mat[newRow][newCol] > mat[row][col] + 1) {
-                    mat[newRow][newCol] = mat[row][col] + 1;
-                    q.push({newRow, newCol});
-                }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
             }
         }
-
-        return mat;        
+        return mat;
     }
 };
